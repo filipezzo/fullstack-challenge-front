9e5c614bf259dc146524dc50d3073edfe3e4c403
Filipe Avanzzo <avanzzodev@gmail.com>
2024-12-26 19:33:15 -0300

    feat: not found home products

--- src/view/pages/home/components/home-list.tsx
+++ src/view/pages/home/components/home-list.tsx
@@ -1,3 +1,4 @@
+import { HeartCrack } from "lucide-react";
 import { Product } from "../../../../app/models/product";
 import { Loading } from "../../../components/loading";
 import { HomeListItem } from "./home-list-item";
@@ -10,6 +11,15 @@ interface HomeListProps {
 export function HomeList({ products, isLoading }: HomeListProps) {
   if (isLoading) {
     <Loading />;
+  }
+
+  if (products.length === 0) {
+    return (
+      <div className="flex items-center justify-center gap-4">
+        <HeartCrack />
+        <p className="text-2xl"> Não encontramos nenhum produto</p>
+      </div>
+    );
   }
   return (
     <ul className="flex flex-wrap justify-center gap-4 lg:grid lg:grid-cols-4 lg:gap-8">
